f0eab7f53dee4412291af1bcaa1856ef7e2df2c5
HopeNux <hopyack@gmail.com>
2013-08-21 17:24:17 +0200

    Create archbox_3drivers.sh
    Drivers [Script passif]

---
+++ archbox_3drivers.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+clear
+echo "***************************************************************"
+echo "* Installation des drivers"
+echo "* Votre carte graphique :"
+lspci -k | grep -A 2 -i "VGA"
+echo "***************************************************************"
+echo ""
+
+echo "***************************************************************"
+echo "* Raspberry PI drivers vidéos"
+echo "* pacman -Su --noconfirm xf86-video-fbdev"
+echo "***************************************************************"
+echo ""
+
+echo "***************************************************************"
+echo "* Nvidia drivers vidéos"
+#----------------------------------------------------------------
+# Drivers NVIDIA --> Config /etc/X11/xorg.conf.d/10-keyboard-layout.conf
+#----------------------------------------------------------------
+# touch /etc/X11/xorg.conf.d/10-keyboard-layout.conf
+# echo "Section \"InputClass\"" >> /etc/X11/xorg.conf.d/10-keyboard-layout.conf
+# echo "	Identifier	\"Keyboard Layout\"" >> /etc/X11/xorg.conf.d/10-keyboard-layout.conf
+# echo "	MatchIsKeyboard	\"yes\"" >> /etc/X11/xorg.conf.d/10-keyboard-layout.conf
+# echo "	MatchDevicePath	\"/dev/input/event*\"" >> /etc/X11/xorg.conf.d/10-keyboard-layout.conf
+# echo "	Option	\"XkbLayout\"	\"fr\"" >> /etc/X11/xorg.conf.d/10-keyboard-layout.conf
+# echo "	Option	\"XkbVariant\"	\"latin9\"" >> /etc/X11/xorg.conf.d/10-keyboard-layout.conf
+# echo "EndSection" >> /etc/X11/xorg.conf.d/10-keyboard-layout.conf
+echo "*  pacman -Su --noconfirm nvidia nvidia-utils lib32-nvidia-utils"
+echo "*  Si conflit libgl avec paquets nvidia utils pacman -Rdd libgl"
+echo "***************************************************************"
+echo ""
+echo "***************************************************************"
+echo "* Nouveau drivers vidéos"
+echo "*  pacman -Su --noconfirm xf86-video-nouveau nouveau-dri"
+echo "*  Acceleration 3D : pacman -Su --noconfirm libva-vdpau-driver"
+echo "*  Generation du Xorg.conf dans /root/xorg.conf.new : X -configure"
+echo "*  TEST :  X -config /root/xorg.conf.new"
+echo "*  mv /root/xorg.conf.new /etc/X11/xorg.conf"
+echo "***************************************************************"
+echo ""
+echo "***************************************************************"
+echo "* AMD drivers vidéos"
+echo "*  pacman -Su --noconfirm xf86-video-ati"
+echo "* AMD drivers Carte mère"
+echo "*  pacman -Su --noconfirm amd-ucode"
+echo "***************************************************************"
+echo ""
+echo "***************************************************************"
+echo "* INTEL drivers vidéos"
+echo "*  pacman -Su --noconfirm xf86-video-intel"
+echo "***************************************************************"
+echo ""
+echo "***************************************************************"
+echo "* ALL drivers vidéos sans acceleration 3D"
+echo "*  pacman -Su --noconfirm xf86-video-vesa"
+echo "***************************************************************"
+echo ""
+echo "***************************************************************"
+echo "* Arch --> Installation Basic --> Drivers --> [Xbmc] --> Check "
+echo "***************************************************************"
+sh _xbmc_install.sh
