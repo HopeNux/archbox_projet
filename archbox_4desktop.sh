6495ba5c7c450d0a60506a46b2c627397329e820
HopeNux <hopyack@gmail.com>
2013-08-26 09:57:11 +0200

    MAJ 3

--- archbox_4desktop.sh
+++ archbox_4desktop.sh
@@ -7,3 +7,6 @@ echo "*# Votre console multimédia de salon 						   #*"
 echo "*# 														   #*"
 echo "***************************************************************"
 echo ""
+pacman -Su --noconfirm xfce4 xfce4-goodies
+pacman -U tools/archbox-theme/archbox-config.tar.xz
+# pacman -Su --noconfirm xfce4-panel xfconf xfce4-clipman-plugin orage xfce4-wavelan-plugin xfce4-mixer gstreamer0.10-base-plugins faenza-icon-theme elementary-gtk-theme
