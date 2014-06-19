f2654762f49cdc4f88595ced886a1ee100483e9c
HopeNux <hopyack@gmail.com>
2014-06-19 11:00:41 +0100

    Interface + Bug boot autologin corrige
    Interface + Bug boot autologin corrige

--- archbox_5drivers.sh
+++ archbox_5drivers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ################################################################################################################
 #------------------------------------------------------------------------------------------------------
-# ARCHBOX_5DRIVERS.SH --> DEBUT
+# ARCHBOX_6BOOT.SH --> DEBUT
 #------------------------------------------------------------------------------------------------------
 archi=`uname -m`
 rep=`(cd $(dirname "$0"); pwd)` 2>/dev/null
@@ -23,10 +23,11 @@ echo -e "$green ****************************************************************
 echo -e "$green * "
 echo -e "$green * [$red ARCHBOX$green ]"									   				  
 echo -e "$green * Votre console multimedia de salon" 
-echo -e "$green * Installation $yellow [DRIVERS]$cyan [En cour...]"
+echo -e "$green * Installation $yellow [BOOT]$cyan [En cour...]"
 echo -e "$green * "
 echo -e "$green ******************************************************************************"
 echo " "
+
 ################################################################################################################
 
 
@@ -34,9 +35,9 @@ echo " "
 #------------------------------------------------------------------------------------------------------
 # Vérification fichier lck
 #------------------------------------------------------------------------------------------------------
-sh $rep/tools/archbox-opt/archbox_error.sh "lck" "$rep/5drivers.lck"
-if [ -f "$rep/5drivers.lck" ] ; then
-	rm $rep/5drivers.lck
+sh $rep/tools/archbox-opt/archbox_error.sh "lck" "$rep/6boot.lck"
+if [ -f "$rep/6boot.lck" ] ; then
+	rm $rep/6boot.lck
 fi
 
 #------------------------------------------------------------------------------------------------------
@@ -75,82 +76,216 @@ sh $rep/tools/archbox-opt/archbox_maj.sh
 
 
 ################################################################################################################
-echo -e "Installation et configuration de vos drivers ... "
-echo -e "Votre carte graphique : $yellow"
-lspci | grep -A 2 -i "VGA"
-echo -e "$green"
+echo -e "Reglage de la carte reseau... $cyan"
+ifconfig -a | more
+
+echo -e "$red"
+read -p "Veuillez saisir le nom de votre carte réseau (eth0) : " cartereseau
+read -p "Souhaitez-vous definir une adresse IP fixe  Oui ? Non ? [Non] : " REP
+
+case $REP in
+	"o"|"oui"|"O"|"Oui"|"OUI"|"y"|"yes"|"Y"|"Yes"|"YES")
+
+		read -p "Adresse IP souhaité : " ip
+		read -p "Adresse du masque de sous réseau (defaut : 255.255.255.0) " mask
+		read -p "Adresse de la passerelle par défaut : " gateway
+
+		if [ -z "$mask" ] ; then
+			mask="255.255.255.0"
+		fi	
+		
+		export ip=$ip
+		echo "Votre ip : "$ip
+		echo "interface="$cartereseau"" > /etc/conf.d/network
+		echo "address="$ip"" >> /etc/conf.d/network
+		echo "netmask="$mask"" >> /etc/conf.d/network
+		echo "broadcast=192.168.1.255" >> /etc/conf.d/network
+		echo "gateway="$gateway"" >> /etc/conf.d/network	
+
+		systemctl disable dhcpcd.service
+		systemctl disable dhcpcd@$cartereseau.service
+		systemctl enable network.service 
+
+		echo -e "$white$ok Configuration ip fixe";;
+	*)		
+		if [ -z "$cartereseau" ] ; then
+			systemctl enable dhcpcd.service			
+		else
+			systemctl enable dhcpcd@$cartereseau.service
+		fi
+		echo -e "$white$ok Configuration dhcpd";;
+esac
+
+cp $rep/tools/archbox-network/network.service /etc/systemd/system/
+
+echo "# ARCHBOX " > /etc/resolv.conf
+echo "# Configuration DNS [1-8.8.8.8 / 2-8.8.4.4]" >> /etc/resolv.conf
+echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+echo "nameserver 8.8.4.4" >> /etc/resolv.conf
+echo "nameserver "$gateway"" >> /etc/resolv.conf
+
+echo -e "$white$ok Configuration réseau & dns"
 
 #------------------------------------------------------------------------------------------------------
-# Liste des drivers (installation pour le rpi)
+# Config /etc/netctl (par defaut)
 #------------------------------------------------------------------------------------------------------
+echo -e "Configuration par defaut de netctl"
+cho -e "$white$ok Configuration netctl"
+
+################################################################################################################
+
+
+################################################################################################################
 if [ "$archi" == "rpi" ] ; then
-	echo -e "Raspberry PI drivers vidéos $cyan"
-	pacman -S xf86-video-fbdev
-	amixer cset numid=0
-else
-	echo -e "Choisissez vos drivers ... $red"
-	LISTE=(" USB/VM (ati + nouveau + intel + vesa) " " AMD/ATI (affiches commentaires) " " NVIDIA (affiches commentaires)" " INTEL (affiches commentaires)" " MESA (install mesa)" " Deja Installé (ne fait rien) ")
-	select CHOIX in "${LISTE[@]}" ; do
-	case $REPLY in
-			1)
-					echo -e "Installation des drivers vidéos par défaut $yellow [USB] $cyan"
-					pacman -S xf86-video-intel xf86-video-nouveau nouveau-dri xf86-video-ati xf86-video-vesa
-			break
-			;;
-			2)
-					echo -e "Lancement :" "$green" " AMD $cyan"
-					echo -e " pacman -S --noconfirm xf86-video-ati$green"
-					echo -e "AMD drivers Carte mère$cyan"
-					echo -e " pacman -S --noconfirm amd-ucode"
-			break
-			;;
-			3)
-					echo -e "Lancement :" "$green" " NVIDIA... $cyan"
-					echo -e " pacman -S --noconfirm nvidia nvidia-utils lib32-nvidia-utils$green"
-					echo -e " Si conflit libgl avec paquets nvidia utils pacman -Rdd libgl"
-
-					echo -e "Nouveau$cyan"
-					echo -e " pacman -S --noconfirm xf86-video-nouveau nouveau-dri$green"
-					echo -e " Acceleration 3D : pacman -S --noconfirm libva-vdpau-driver"
-					echo -e " Generation du Xorg.conf dans /root/xorg.conf.new : X -configure"
-					echo -e " TEST :  X -config /root/xorg.conf.new"
-					echo -e " mv /root/xorg.conf.new /etc/X11/xorg.conf"
-			break
-			;;
-			4)
-					echo -e "Lancement :" "$green" " INTEL... "
-					echo -e "INTEL drivers vidéos$cyan"
-					pacman -S --noconfirm xf86-video-intel
-			break
-			;;
-			5)
-					echo -e "Lancement :" "$green" " MESA "
-					echo -e "ALL drivers vidéos sans acceleration 3D $cyan"
-					pacman -S --noconfirm xf86-video-vesa
-			break
-			;;
-			6)
-					echo -e "[DEJA INSTALLE] $nc"
-			break
-			;;
-			esac
-	done	
+	ln -s /usr/lib/systemd/system/serial-getty@.service /etc/systemd/system/getty.target.wants/serial-getty@ttyAMA0.service
 fi
-echo -e "$ok$white Drivers configurer."
 
-cat <<EOF > $rep/5drivers.lck
-#-----------------------------
-# ARCHBOX_5DRIVERS.SH --> [OK]
-#-----------------------------
+#------------------------------------------------------------------------------------------------------
+# Install autologin
+#------------------------------------------------------------------------------------------------------
+echo -e "Installation de l'autologin sur $user"
+echo -e "Copie /usr/lib/systemd/system/autologin.service"
+if [ -f "$rep/tools/archbox-boot/autologin@.service" ] ; then
+	rm $rep/tools/archbox-boot/autologin@.service
+fi
+cat <<EOF > $rep/tools/archbox-boot/autologin@.service
+[Unit]
+Description=Getty on %I
+Documentation=man:agetty(8) man:systemd-getty-generator(8)
+Documentation=http://0pointer.de/blog/projects/serial-console.html
+After=systemd-user-sessions.service plymouth-quit-wait.service
+
+# If additional gettys are spawned during boot then we should make
+# sure that this is synchronized before getty.target, even though
+# getty.target didn't actually pull it in.
+Before=getty.target
+IgnoreOnIsolate=yes
+
+# On systems without virtual consoles, don't start any getty. (Note
+# that serial gettys are covered by serial-getty@.service, not this
+# unit
+ConditionPathExists=/dev/tty0
+
+[Service]
+# the VT is cleared by TTYVTDisallocate
+ExecStart=-/sbin/agetty --noclear -a $user %I 38400 linux
+Type=idle
+Restart=always
+RestartSec=0
+UtmpIdentifier=%I
+TTYPath=/dev/%I
+TTYReset=yes
+TTYVHangup=yes
+TTYVTDisallocate=yes
+KillMode=process
+IgnoreSIGPIPE=no
+
+# Unset locale for the console getty since the console has problems
+# displaying some internationalized messages.
+Environment=LANG= LANGUAGE= LC_CTYPE= LC_NUMERIC= LC_TIME= LC_COLLATE= LC_MONETARY= LC_MESSAGES= LC_PAPER= LC_NAME= LC_ADDRESS= LC_TELEPHONE= LC_MEASUREMENT= LC_IDENTIFICATION=
+
+# Some login implementations ignore SIGTERM, so we send SIGHUP
+# instead, to ensure that login terminates cleanly.
+KillSignal=SIGHUP
+
+[Install]
+Alias=getty.target.wants/getty@tty1.service
+EOF
+cp $rep/tools/archbox-boot/autologin@.service /usr/lib/systemd/system/getty@.service
+cp $rep/tools/archbox-boot/autologin@.service /etc/systemd/system/
+cho -e "$white$ok Autologin sur $user"
+
+#------------------------------------------------------------------------------------------------------
+# Install archboxboot
+#------------------------------------------------------------------------------------------------------
+echo -e "Copie /usr/bin/archboxboot ..."
+cp $rep/tools/archbox-boot/archboxboot /usr/bin/
+chmod 755 /usr/bin/archboxboot
+echo -e "Copie /etc/systemd/system/archbox.service ..."
+cp $rep/tools/archbox-boot/archbox@.service /etc/systemd/system/
+echo -e "$white$ok ARCHBOX Service (voir /usr/bin/archboxboot pour le script de lancement) "
+
+#------------------------------------------------------------------------------------------------------
+# Config xinitrc
+#------------------------------------------------------------------------------------------------------
+if [ -f "$HOME/.xinitrc" ] ; then
+	rm $HOME/.xinitrc
+fi
+cat <<EOF >$HOME/.xinitrc
+#!/bin/sh
+# ~/.xinitrc
+# Executed by startx (run your window manager from here)
+if [ -d /etc/X11/xinit/xinitrc.d ]; then
+    for f in /etc/X11/xinit/xinitrc.d/*; do
+	    [ -x "$f" ] && . "$f"
+	done
+	unset f
+fi
+# exec startxfce4
+# exec xbmc
+EOF
+echo -e "$white$ok Config .xinitrc "
+
+#------------------------------------------------------------------------------------------------------
+# Config bash_profile
+#------------------------------------------------------------------------------------------------------
+if [ -f "$HOME/.bash_profile" ] ; then
+	rm $HOME/.bash_profile
+fi
+cat <<EOF >$HOME/.bash_profile
+# ~/.bash_profile
+#
+#[[ -f ~/.bashrc ]] && . ~/.bashrc
+#[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx
+EOF
+echo "sh /usr/bin/archboxboot" >> /home/$user/.bash_profile
+echo -e "$white$ok Config .bash_profile (ajout de script archboxboot au demarrage) "
+
+#------------------------------------------------------------------------------------------------------
+# Config xsession
+#------------------------------------------------------------------------------------------------------
+if [ -f "$HOME/.xsession" ] ; then
+	rm $HOME/.xsession
+fi
+cat <<EOF >$HOME/.xsession
+#!/bin/sh
+# ~/.xsession
+# Executed by xdm/gdm/kdm at login
+/bin/bash --login -i ~/.xinitrc
+EOF
+echo -e "$white$ok Config .xsession  "
+
+#------------------------------------------------------------------------------------------------------
+# Services au demarrage
+#------------------------------------------------------------------------------------------------------
+echo -e "Activation des services au démarrage ..."
+# systemctl enable archbox@xbmc.service <-- PAS DE ARCHBOXBOOT
+systemctl enable upower
+systemctl disable getty@tty1
+systemctl daemon-reload
+systemctl enable autologin@$user.service
+systemctl enable multi-user.target
+systemctl enable devmon@$user
+if [ "$archi" == "rpi" ] ; then
+	systemctl enable getty@ttyAMA0.service	
+fi
+echo -e "$white$ok Activation des services"
+
+cat <<EOF > $rep/6boot.lck
+#----------------------------
+# ARCHBOX_6BOOT.SH --> [OK]
+#----------------------------
 EOF
 ################################################################################################################
 
 
 ################################################################################################################
-echo -e "$green ******************************************************************************"
+echo -e "$green **********************************************************************************************************"
 echo -e "$green * "
-echo -e "$green * [$red ARCHBOX$green ]"					
-echo -e "$green * Installation $yellow [DRIVERS]$red Terminé"				   				  
+echo -e "$green * [$red ARCHBOX$green ]"			
+echo -e "$green * Installation $yellow [BOOT]$red Terminé"						   				  
 echo -e "$green * "
-echo -e "$green ******************************************************************************$nc"
+echo -e "$red * Attention si ce n'ai pas déjà fait ajouter manuellement 'bna' dans $MODULES "
+echo -e "$red * sur /etc/mkinitcpio.conf et recharger mkinitcpio avec ==> mkinitcpio -p linux"
+echo -e "$green ********************************************************************************************************** $nc"
 ################################################################################################################
