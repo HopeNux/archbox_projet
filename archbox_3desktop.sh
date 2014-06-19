ca8ded3c3c9cc14734506643c4762b4248874730
HopeNux <hopyack@gmail.com>
2014-06-19 10:58:38 +0100

    Nouveau skins, habillage par defaut(Numix), Interface bureautique change, raspberry install
    Nouveau skins, habillage par defaut(Numix), Interface bureautique change, raspberry install

--- archbox_3desktop.sh
+++ archbox_3desktop.sh
@@ -1,10 +1,12 @@
-﻿#!/bin/bash
-###############################################################################################
+#!/bin/bash
+################################################################################################################
 #----------------------------------------------------------------
 # ARCHBOX_3DESKTOP.SH --> DEBUT
 #----------------------------------------------------------------
+archi=`uname -m`
 rep=`(cd $(dirname "$0"); pwd)` 2>/dev/null
-export LANG=fr_FR.UTF-8
+export LANG="fr_FR.UTF-8"
+export keymap="fr-pc"
 export blue="\\033[1;34m"
 export cyan="\\033[1;36m"
 export green="\\033[1;32m"
@@ -12,20 +14,23 @@ export nc="\\033[0;39m"
 export red="\\033[1;31m"
 export white="\\033[1;37m"
 export yellow="\\033[1;33m"
-###############################################################################################
+export ok="[ $yellow OK $white ]$green"
 
 echo " "
 echo " "
 
-###############################################################################################
 echo -e "$green ******************************************************************************"
 echo -e "$green * "
 echo -e "$green * [$red ARCHBOX $green ]"									   				  
 echo -e "$green * Votre console multimedia de salon" 
-echo -e "$green * Installation $yellow [DESKTOP XFCE]$cyan [En cour...]"
+echo -e "$green * Installation $yellow [DESKTOP XFCE/LXDE(RPI)]"
 echo -e "$green * "
 echo -e "$green ******************************************************************************"
-echo -e "$green * "
+echo " "
+################################################################################################################
+
+
+################################################################################################################
 #----------------------------------------------------------------
 # Vérification fichier lck
 #----------------------------------------------------------------
@@ -35,24 +40,10 @@ if [ -f "$rep/3desktop.lck" ] ; then
 fi
 
 #----------------------------------------------------------------
-# Mise à jour + Installation
-#----------------------------------------------------------------
-sh $rep/tools/archbox-opt/archbox_maj.sh
-echo -e "$green * "
-echo -e "$green ******************************************************************************"
-###############################################################################################
-
-echo " "
-echo " "
-
-###############################################################################################
-echo -e "$white ******************************************************************************"
-echo -e "$white * Installation et configuration de XFCE $red"
-#----------------------------------------------------------------
 # Nouveau utilisateur
 #----------------------------------------------------------------
 if [ -z "$1" ] ; then
-	read -p " * (2) Nouveau utilisateur : (défaut xbmc) " user
+	read -p "Nouveau utilisateur : (défaut xbmc) " user
 	if [ -z "$user" ] ; then
 		user="xbmc"
 	fi
@@ -61,72 +52,122 @@ else
 fi
 export HOME="/home/$user"
 
-#----------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------
 # Architecture (i386 - i686 - x86_64 - armv6l)
-#----------------------------------------------------------------
-if [ -z "$2" ] ; then
-	archi=`uname -m`
-	echo -e "$green * Votre architecture$yellow $archi"
-	if [ "$archi" = "armv6l" ] ; then
-		echo -e " * $red"
-		read -p " * Votre machine est elle un Raspberry Pi Oui ? Non ? [def:Non] : " rpi
-		case $rpi in
-			"o"|"oui"|"O"|"Oui"|"OUI"|"y"|"yes"|"Y"|"Yes"|"YES")
-				archi="rpi" ;; 
-			*)
-				echo "$green * " ;;
-		esac
-	fi
-else
-	echo -e "$white * Architecture $2"
+#------------------------------------------------------------------------------------------------------
+echo -e "Votre architecture : $cyan$archi"
+echo -e "$red"
+if [ "$archi" = "armv6l" ] ; then
+	read -p "Votre machine est elle un Raspberry Pi Oui ? Non ? [def:Non] : " rpi
+	case $rpi in
+		"o"|"oui"|"O"|"Oui"|"OUI"|"y"|"yes"|"Y"|"Yes"|"YES")
+			archi="rpi" ;; 
+		*)
+			echo "  " ;;
+	esac
 fi
 
 #----------------------------------------------------------------
-# Installation XFCE
+# Mise à jour + Installation
 #----------------------------------------------------------------
-# A ENLEVER : nepomuk
-echo -e "$white * $cyan"
+sh $rep/tools/archbox-opt/archbox_maj.sh
+################################################################################################################
+
+echo " "
+echo " "
+
+################################################################################################################
+#----------------------------------------------------------------
+# Installation XFCE / LXDE (RPI)
+#----------------------------------------------------------------
+echo -e "Installation et configuration de XFCE ... $cyan"
 pacman -S --noconfirm xfce4 
-pacman -S --noconfirm xfce4-goodies hardinfo gsmartcontrol uget wget
-pacman -S --noconfirm gvfs # montage de disque réseau avec thunar.
-pacman -S --noconfirm gvfs-smb # gestionnaire de fichier avec prise en charge réseau.
-pacman -S --noconfirm gvfs-afc # Montage smartphone
-pacman -S --noconfirm gvfs-mtp # Montage tablette ou appareil photo
-pacman -S --noconfirm thunar-archive-plugin #  ajoute une entrée dans le menu contextuel (clic droit) pour créer/décompresser les archives depuis Thunar.
-pacman -S --noconfirm thunar-volman # pour activer la gestion automatique des disques et médias amovibles.
-pacman -S --noconfirm gvfs-gphoto2 gvfs-afp gvfs-goa
-pacman -S --noconfirm file-roller lrzip unace unrar p7zip thunar-archive-plugin # Gestionaires d'archives
-pacman -S --noconfirm xdg-user-dirs lftp
-pacman -S --noconfirm acpi acpid # Gestionnaire d'energie
-pacman -S --noconfirm kde-l10n-fr # Avoir les soft kde en fr
-pacman -S --noconfirm kdegraphics-kolourpaint # Soft très proche de MSpaint (kde)
-pacman -S --noconfirm kaffeine vlc mpv # Lecteurs multimedias
-pacman -S --noconfirm gnome-calculator # Calculatrice ( très important :p )
-pacman -S --noconfirm mupdf firestarter # Lecteur pdf simple + parfeu
+pacman -S --noconfirm xfce4-goodies hardinfo gsmartcontrol uget pavucontrol xfce-theme-manager
 pacman -S --noconfirm faenza-icon-theme # Pack icones faenza / gstreamer0.10-base gstreamer0.10-base-plugins
-pacman -S --noconfirm firefox firefox-i18n-fr #Firefox en FR
-echo -e "$yellow * [Attention on passe au dessert(yaourt) ahah-ah...!] $cyan"
-pacman -Syy
-yaourt -S --noconfirm chromium # Chromium libre
-yaourt -S --noconfirm chromium-pepper-flash-stable # Chromium libre flash libre
-yaourt -S --noconfirm brocade-firmware # Résout l'erreur "WARNING: Possibly missing firmware for module: bfa" ---> AJOUTER COMMENTAIRE EN FIN MODULES="bna" /etc/mkcinit...conf 
-# Please add the 'bna' module to $MODULES in /etc/mkinitcpio.conf and rebuild your initcpio to complete the installation
-# execut mkinitcpio -p linux
-yaourt -S --noconfirm aic94xx-firmware # Résout l'erreur "WARNING: Possibly missing firmware for module: aic94xx"
-yaourt -S --noconfirm siano-tv-fw # Résout l'erreur "WARNING: Possibly missing firmware for module: smsmdtv"
-yaourt -S --noconfirm xfce-theme-manager ttf-roboto gtk-theme-numix-holo gtk-theme-numix-git numix-icon-theme-git gtk-theme-numix-blue gtk-theme-numix-white gtk-theme-numix-green xcursors-oxygen # themes numix
+echo -e "$white$ok Interface XFCE $cyan"
+
+#----------------------------------------------------------------
+# Installation des outils de base pour l'interface graphique
+#----------------------------------------------------------------
+# gvfs 			- Montage de disque réseau avec thunar.
+# gvfs-smb		- Gestionnaire de fichier avec prise en charge réseau.
+# gvfs-afc		- Montage smartphone
+# gvfs-mtp		- Montage tablette ou appareil photo
+# archive-plugin- Ajoute une entrée dans le menu contextuel (clic droit) pour créer/décompresser les archives depuis Thunar.
+# volman		- Pour activer la gestion automatique des disques et médias amovibles.
+pacman -S --noconfirm gvfs gvfs-smb gvfs-afc gvfs-mtp gvfs-gphoto2 gvfs-afp gvfs-goa
+pacman -S --noconfirm thunar-archive-plugin thunar-volman
+echo -e "$white$ok Thunar et plugins $cyan" 
+
+pacman -S --noconfirm p7zip # Gestionaires d'archives
+echo -e "$white$ok Gestionaire d'archive $cyan"
+
+# Avoir les soft kde en fr
+# Soft très proche de MSpaint (kde)
+# Calculatrice ( très important :p )
+# Lecteurs multimedias
+pacman -S --noconfirm kde-l10n-fr kdegraphics-kolourpaint kaffeine gnome-calculator vlc mpv mousepad
+echo -e "$white$ok Paint - Lecteur video/musique - calculatrice $cyan"
+
+pacman -S --noconfirm firefox firefox-i18n-fr subversion midori
+echo -e "$white$ok Navigateur $cyan"
+
+pacman -S --noconfirm firestarter acpi acpid # Gestionnaire d'energie
+echo -e "$white$ok Parfeu (firestarter) et Gestionnaire d'energie $cyan"
+
+# D-Bus			- D-bux is a message bus system that provides an easy way for inter-process communication. / D-Bus is enabled automatically when using systemd because dbus is a dependency of systemd.
+# Python/2		- Python/2 bindings for the cairo graphics library
+pacman -S --noconfirm dbus dbus-python python-cairo python2-cairo 
+echo -e "$white$ok Gestionnaire d'energie - dbus - python $yellow"
+
+echo -e "[Attention on passe au dessert(yaourt) ahah-ah...!] $cyan"
+yaourt -Syy
 yaourt -S --noconfirm foxitreader-bin pacmanxg4-bin # lecteur pdf + gestionnaire de paquet
-echo -e "$yellow * "
+echo -e "$white$ok foxitreader pacmanxg4(interface de mise a jour) $cyan"
+#----------------------------------------------------------------
+# Installation des firmware
+#----------------------------------------------------------------
+# brocade-firmware		- Résout l'erreur "WARNING: Possibly missing firmware for module: bfa" ---> AJOUTER COMMENTAIRE EN FIN MODULES="bna" /etc/mkcinit...conf 
+# 						  Please add the 'bna' module to $MODULES in /etc/mkinitcpio.conf and rebuild your initcpio to complete the installation
+# 						  execut mkinitcpio -p linux
+# aic94xx-firmware		- Résout l'erreur "WARNING: Possibly missing firmware for module: aic94xx
+# siano-tv-fw			- Résout l'erreur "WARNING: Possibly missing firmware for module: smsmdtv
+yaourt -S --noconfirm brocade-firmware siano-tv-fw aic94xx-firmware
+echo -e "$white$ok brocade aic94xx siano-tv (mkinitcpio) $cyan"
+#----------------------------------------------------------------
+# Installation themes pour XFCE
+#----------------------------------------------------------------
+yaourt -S --noconfirm numix-icon-theme-git xcursors-oxygen gtk-theme-boje xfce-theme-numix-extra-colors ttf-roboto
+echo -e "$green"
 mkdir /link/Jeux
-systemctl enable acpid.service
-echo -e "$white * Interface sur l'utilisateur $user $yellow [OK]"
-echo -e "$white ******************************************************************************"
-###############################################################################################
+#systemctl enable acpid.service
+systemctl enable dbus.service
+
+#------------------------------------------------------------------------------------------------------
+# Config themes  user .bashrc
+#------------------------------------------------------------------------------------------------------
+cp $rep/tools/archbox-theme/bashrc /home/$user/.bashrc
+cp $rep/tools/archbox-theme/gtkrc-2.0 /home/$user/.gtkrc-2.0
+cp -R $rep/tools/archbox-theme/xfce4 /home/$user/.config/
+cp -R $rep/tools/archbox-theme/Mousepad /home/$user/.config/
+cp -R $rep/tools/archbox-theme/archbox /usr/share/
+cp -R $rep/tools/archbox-theme/desktop /home/$user/Desktop
+echo -e "$white$ok Interface sur l'utilisateur $user"
+
+chown -R $user:users /home/$user/.bashrc /home/$user/.gtkrc-2.0 /home/$user /home/$user/.config /usr/share/archbox
+chmod -R 755 /home/touriste/.bashrc /home/$user/.bashrc /home/$user/.gtkrc-2.0 /home/$user/.config /usr/share/archbox
+
+cat <<EOF > $rep/3desktop.lck
+#-----------------------------
+# ARCHBOX_3DESKTOP.SH --> [OK]
+#-----------------------------
+EOF
+################################################################################################################
 
 echo " "
 echo " "
 
-###############################################################################################
+################################################################################################################
 #----------------------------------------------------------------
 # ARCHBOX_3DESKTOP.SH --> FIN
 #----------------------------------------------------------------
@@ -138,10 +179,5 @@ echo -e "$green * "
 echo -e "$red * Attention ajouter manuellement 'bna' dans $MODULES sur /etc/mkinitcpio.conf "
 echo -e "$red * et recharger mkinitcpio avec ==> mkinitcpio -p linux"
 echo -e "$green ****************************************************************************** $nc"
-cat <<EOF > $rep/3desktop.lck
-#----------------------------------------------------------------
-# ARCHBOX_3DESKTOP.SH --> [OK]
-#----------------------------------------------------------------
-EOF
-###############################################################################################
+################################################################################################################
 
